299fbef644f8c82e1856dca7aae299c6744a4a90
Lévy <prakkmak@gmail.com>
2024-04-28 15:17:52 +0200

    leaderboard ok

--- screens/leaderboard_screen/leaderboard_screen.gd
+++ screens/leaderboard_screen/leaderboard_screen.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
 @export var base_url: String = "https://luxuriant-longhaired-silk.glitch.me"
-@export var player_score_scene: PlayerScore
+@export var player_score_scene: PackedScene
 
 
 @onready var http_request: HTTPRequest = $HTTPRequest
+@onready var score_container: VBoxContainer = %ScoresContainer
+
 
 
 func _ready() -> void:
@@ -17,4 +19,22 @@ func _load_leaderboard() -> void:
 
 
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	print(JSON.parse_string(body.get_string_from_utf8()))
+	var json: Dictionary = JSON.parse_string(body.get_string_from_utf8())
+	if json["success"] == false:
+		push_warning("Error HTTP")
+		return
+	print(json)
+	var data: Array = json["data"]
+	for player_score: Dictionary in data:
+		_add_player_score(player_score)
+
+func _add_player_score(player_score: Dictionary) -> void:
+	if !player_score_scene:
+		return
+	var new_player_score: PlayerScore = player_score_scene.instantiate() as PlayerScore
+	score_container.add_child(new_player_score)
+	var player_username: String = player_score["username"]
+	var player_id: int = player_score["id"]
+	var player_score_amount: int = player_score["score"]
+	new_player_score.fill(player_id, player_username, player_score_amount)
+	
--- screens/leaderboard_screen/player_score/player_score.gd
+++ screens/leaderboard_screen/player_score/player_score.gd
@@ -5,7 +5,8 @@ extends HBoxContainer
 @onready var player_name_label: Label = %PlayerNameLabel
 @onready var player_score_label: Label = %PlayerScoreLabel
 
-func fill_score(position: int, name: String, score: int) -> void:
+
+func fill(position: int, name: String, score: int) -> void:
 	player_position_label.text = str(position)
 	player_name_label.text = name
 	player_score_label.text = str(score)
