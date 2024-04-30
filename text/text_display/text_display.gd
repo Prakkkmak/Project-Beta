6a4b29a7e9aa1ce93607ffb2d328bcace74a9114
Hiily <hiily1378@gmail.com>
2024-04-30 12:06:26 +0200

    text_display scene

--- text/text_display/text_display.gd
+++ text/text_display/text_display.gd
@@ -1,18 +1,51 @@
 extends Marker2D
 
-#Reference au label (Avec le %)
+@onready var label: Label = %Label
+@onready var timer: Timer = %Timer
+
+@export var texts_to_diplay: Array[String] = []
+
 
 func _ready() -> void:
 	GlobalEvents.threshold_triggered.connect(_on_threshold_triggered)
+	timer.timeout.connect(_on_timer_timeout)
 
-#TODO Fonction publique pour affiher un texte ((comme display_text(text: String))
-#TODO Assign text to Text label
-#TODO Text display fais sa tambouille pour afficher le texte
+
+func display_text(text: String) -> void:
+	print(text)
+	_assign_text_to_label(text)
+	_animate_text_display()
+
+
+func process_next_step() -> void:
+	if texts_to_diplay.is_empty():
+		timer.stop()
+		label.hide()
+		return
+	display_text(texts_to_diplay[0])
+	texts_to_diplay.remove_at(0)
+	if timer.is_stopped(): 
+		timer.start()
+
+
+func _assign_text_to_label(text: String) -> void:
+	label.text = text
+
+
+func _animate_text_display() -> void:
+	label.show()
+ 
 
 func _on_threshold_triggered(event_treshold: EventThreshold) -> void:
 	if !event_treshold is TextEventThreshold:
 		return
 	var text_event_treshold: TextEventThreshold = event_treshold as TextEventThreshold
-	print(text_event_treshold.texts_to_display)
-	#TODO Afficher le texte grace a la fonction
+	texts_to_diplay.append_array(text_event_treshold.texts)
+	process_next_step()
+
+
+func _on_timer_timeout() -> void:
+	print("timeout")
+	process_next_step()
+
 
--- threshold/event_threshold/TextEventThreshold.gd
+++ threshold/event_threshold/TextEventThreshold.gd
@@ -1,4 +1,4 @@
 class_name TextEventThreshold
 extends EventThreshold
 
-@export var texts_to_display: Array[String]
+@export var texts: Array[String]
