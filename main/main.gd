0b0648df1bff292ca4bab4d1691a6c86b79cbeed
Hiily <hiily1378@gmail.com>
2024-05-01 21:18:12 +0200

    localization

--- main/main.gd
+++ main/main.gd
@@ -6,9 +6,14 @@ extends Node
 
 @export_range(0,1000) var starting_score: int = 0
 
+@onready var french_button: Button = %FrenchButton
+@onready var english_button: Button = %EnglishButton
 
 @onready var give_up_button: GiveUpButton = %GiveUpButton
 @onready var quest_generator: QuestGenerator = $QuestGenerator
+
+var languageNames:Array = ["English", "Français"]
+var languageCodes:Array = ["en","fr"]
 
 
 func _ready() -> void:
@@ -16,6 +21,8 @@ func _ready() -> void:
 		return
 	give_up_button.released.connect(_on_give_up_button_pressed)
 	Score.change_score(starting_score)
+	french_button.pressed.connect(_on_french_button_pressed)
+	english_button.pressed.connect(_on_english_button_pressed)
 
 
 func _on_give_up_button_pressed() -> void:
@@ -23,3 +30,17 @@ func _on_give_up_button_pressed() -> void:
 		push_warning("No end screen file path set")
 		return
 	get_tree().change_scene_to_file(end_screen_file_path)
+	
+
+func _on_french_button_pressed() -> void:
+	TranslationServer.set_locale("fr")
+	print("fr")
+	updateUI()
+
+func _on_english_button_pressed() -> void:
+	TranslationServer.set_locale("en")
+	print("en")
+	updateUI()
+
+func updateUI() -> void:
+	pass
