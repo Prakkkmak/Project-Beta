259525ec9f767cca4e809101d91e028f6118cbfc
Hiily <hiily1378@gmail.com>
2024-05-04 21:51:23 +0200

    auton incr stop + title screen improve

--- screens/title_screen/title_screen.gd
+++ screens/title_screen/title_screen.gd
@@ -1,16 +1,26 @@
 extends CanvasLayer
 
-@export_file("*.tscn") var end_screen_file_path: String = "res://scenes/main/main.tscn"
+@export_file("*.tscn") var main_screen_file_path: String = "res://main/main.tscn"
+@export_file("*.tscn") var leaderboard_screen_file_path: String = "res://screens/leaderboard_screen/leaderboard_screen.tscn"
 
-@onready var start_button: Button = %StartButton
+@onready var start_button: TextureButton = %StartButton
+@onready var leaderboard_button: TextureButton = %LeaderboardButton
 
 
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_button_pressed)
+	leaderboard_button.pressed.connect(_on_leaderboard_button_pressed)
 
 
 func _on_start_button_pressed() -> void:
-	if !end_screen_file_path:
+	if !main_screen_file_path:
 		push_warning("No end screen file path set")
 		return
-	get_tree().change_scene_to_file(end_screen_file_path)
+	get_tree().change_scene_to_file(main_screen_file_path)
+
+
+func _on_leaderboard_button_pressed() -> void:
+	if !leaderboard_screen_file_path:
+		push_warning("No end screen file path set")
+		return
+	get_tree().change_scene_to_file(leaderboard_screen_file_path)
