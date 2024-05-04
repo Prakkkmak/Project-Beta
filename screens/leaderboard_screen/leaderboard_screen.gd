cb2d7239202fb6d56c1de6ba3b9edaeb6c37cd7f
Hiily <hiily1378@gmail.com>
2024-05-04 23:50:06 +0200

    leaderboard improve

--- screens/leaderboard_screen/leaderboard_screen.gd
+++ screens/leaderboard_screen/leaderboard_screen.gd
@@ -1,11 +1,15 @@
 extends CanvasLayer
 
+@export_file("*.tscn") var title_screen_file_path: String = "res://screens/title_screen/title_screen.tscn"
+
 @export var player_score_scene: PackedScene
 @onready var score_container: VBoxContainer = %ScoresContainer
+@onready var main_menu_button: TextureButton = %MainMenuButton
 
 func _ready() -> void:
 	_add_player_scores()
 	Leaderboard.player_scores_loaded.connect(_on_player_scores_loaded)
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
 
 func _add_player_scores() -> void:
 	for player_score: Dictionary in Leaderboard.player_scores_sorted:
@@ -22,3 +26,10 @@ func _on_player_scores_loaded() -> void:
 		score_container.remove_child(node)
 		node.queue_free()
 	_add_player_scores()
+
+func _on_main_menu_button_pressed() -> void:
+	if !title_screen_file_path:
+		push_warning("No end screen file path set")
+		return
+	get_tree().change_scene_to_file(title_screen_file_path)
+
